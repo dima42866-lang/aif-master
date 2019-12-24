f378dffd91b097c1a93aa063d8d8b05145b926d3
Doctor_Who <maximalis171091@yandex.ru>
2019-12-24 15:16:56 +0300

    Fix bug pacman.conf

--- aif-installation/modules/installation-functions.sh
+++ aif-installation/modules/installation-functions.sh
@@ -6,18 +6,33 @@
 ##                                                                  ##
 ######################################################################  
 
+pc_conf_prcss()
+{
+    echo "#" > "$2"
+    while read line; do
+        if [[ $line == "#[multilib]" ]]; then
+            _next=1
+            echo "[multilib]" >> "$2"
+            echo "Include = /etc/pacman.d/mirrorlist" >> "$2"
+        else
+            if [[ $_next == "1" ]]; then
+                _next=0
+                continue
+            else
+                echo "$line" >> "$2"
+            fi
+        fi
+    done < "./$1"
+    cp -f "$2" "$1"
+}
+
 multilib_question()
 {
     dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title "$_yesno_multilib_title" --yesno "$_yesno_multilib_body" 0 0
 
     if [[ $? -eq 0 ]]; then
-        # echo "[multilib]" >> /etc/pacman.conf
-        # echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-        sed -i 's/^\#\[multilib\]$/[multilib]/' /etc/pacman.conf
-        sed -i '/^\[multilib\]$/a\Include = /etc/pacman.d/mirrorlist' /etc/pacman.conf
-        # sed -i '93s/\#\[multilib\]/[multilib]/' /etc/pacman.conf
-        # sed -i '94s/\#Include/Include/' /etc/pacman.conf
-        _multilib=1
+       pc_conf_prcss "$_pcm_conff" "$_pcm_tempf"
+       _multilib=1
     else
         _multilib=0
     fi
