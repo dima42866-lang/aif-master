5aea3d0a64ccf5ed1904aab25321a2f657f97bb7
Doctor_Who <maximalis171091@yandex.ru>
2020-04-19 22:39:51 +0300

    new_info

--- aif-installation/rd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-_data_file_name="./data-files"
-[ -e $_data_file_name ] && source $_data_file_name
-_info_array=( $_your_data )
-unset _your_data
-echo "${_info_array[1]}" | sudo -u ${_info_array[0]} -S systemctl is-active NetworkManager
-exit 0
